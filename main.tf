0ef414a3b5692f17ccb8a88779ba529db448a009
errolt24 <errol.thomas@mavenwave.com>
2022-09-07 13:34:14 -0500

    new network names

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ zone = "us-central1-c"
 }
 
 resource "google_compute_network" "vpc_network" {
-name = "terraform-network"
+name = "terraform-network-junior"
 }
 
 resource "google_compute_subnetwork" "public-subnetwork" {
-name = "terraform-subnetwork"
+name = "terraform-subnetwork-junior"
 ip_cidr_range = "10.2.0.0/16"
 region = "us-central1"
 network = google_compute_network.vpc_network.name
